b05d8b09d39d53c859650574c0fd1edafd055241
Pratik Raj Verma <erpratikrajverma@gmail.com>
2024-08-04 12:32:25 +0530

    new branch

--- Server/index.js
+++ Server/index.js
@@ -4,8 +4,8 @@ const app = express();
 const cors = require('cors');
 app.use(cors({
     // origin: 'http://localhost:3000',
-    origin: '*',
     
+    origin: '*',
     credentials:true,
 }));
 
